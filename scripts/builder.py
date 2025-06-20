2527280f2e2e84eca69388a87ecd73d3c66801e3
Ashwin Vinod <ashwinvinodsa@gmail.com>
2025-06-20 21:45:49 +0530

    Implement a better site generator

---
+++ scripts/builder.py
@@ -0,0 +1,97 @@
+import argparse
+import logging
+import shutil
+from dataclasses import dataclass
+from pathlib import Path
+
+from jinja2 import DictLoader, Environment, select_autoescape
+
+from parser import TemplateManager
+
+
+@dataclass
+class Builder:
+    source_dir: Path
+    template_dir: Path
+    output_dir: Path
+    assets_dir: Path
+
+    def prepare(self):
+        template_managers: list[TemplateManager] = []
+        template_store = {}
+
+        template_renderer = Environment(
+            loader=DictLoader(template_store), autoescape=select_autoescape()
+        )
+
+        for file in self.template_dir.glob("*.html"):
+            template_store[file.stem] = file.read_text()
+            if manager := TemplateManager.create(file, self.source_dir):
+                template_managers.append(manager)
+
+        # Base files must be html (for now)
+        base_files = [file for file in self.source_dir.iterdir() if file.is_file()]
+
+        logging.info("Found %d templates", len(template_managers))
+        logging.info("Found %d base files", len(base_files))
+
+        for file in base_files:
+            template_store[file.name] = file.read_text()
+
+        return template_renderer, template_managers, base_files
+
+    def build(self):
+        if not self.output_dir.exists():
+            self.output_dir.mkdir(parents=True)
+
+        template_renderer, templates, base_files = self.prepare()
+
+        mappings = {}
+        mappings["category_links"] = [template.link for template in templates]
+        mappings["base_links"] = ["/" + file.name for file in base_files]
+
+        shutil.copytree(self.assets_dir, self.output_dir / "assets", dirs_exist_ok=True)
+
+        # Render base files
+        for file in base_files:
+            template = template_renderer.get_template(file.name)
+            content = template.render(mappings)
+            output_path = self.output_dir / file.name
+            output_path.write_text(content)
+        logging.info("Finished rendering base files")
+
+        for manager in templates:
+            # Copy template style files to assets folder
+            if style_path := manager.get_style_file:
+                shutil.copy(style_path, self.output_dir / "assets" / style_path.name)
+
+            for file in manager.render_files(template_renderer, mappings):
+                output_path = self.output_dir / file.output_relative_path
+                # write_text fn fails if the parent folders doesn't exist
+                if not output_path.parent.exists():
+                    output_path.parent.mkdir(parents=True)
+
+                output_path.write_text(file.contents)
+        logging.info("Build complete. Output files written to %s", self.output_dir)
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser("Basic Static Site Generator")
+    arg_parser.add_argument(
+        "--src-dir", help="Path to directory containing source files"
+    )
+    arg_parser.add_argument("--assets-dir", help="Path to directory containing assets")
+    arg_parser.add_argument(
+        "--templates-dir", help="Path to directory containing template files"
+    )
+    arg_parser.add_argument("--output-dir", help="Path to output directory")
+
+    args = arg_parser.parse_args()
+
+    src_dir = Path(args.src_dir).absolute()
+    assets_dir = Path(args.assets_dir).absolute()
+    template_dir = Path(args.templates_dir).absolute()
+    output_dir = Path(args.output_dir).absolute()
+
+    builder = Builder(src_dir, template_dir, output_dir, assets_dir)
+    builder.build()
---
+++ scripts/parser.py
@@ -0,0 +1,108 @@
+import logging
+import os
+import typing
+from dataclasses import dataclass
+from functools import cache
+from pathlib import Path
+from typing import Dict
+
+from jinja2 import Environment
+from markdown_it import MarkdownIt
+
+md_renderer = MarkdownIt("gfm-like")
+
+
+@dataclass
+class RenederedTemplate:
+    output_relative_path: Path
+    contents: str
+
+    @classmethod
+    def create(cls, src_relative_path: Path, contents: str):
+        file_name = src_relative_path.stem.lower().replace(" ", "_")
+        return RenederedTemplate(
+            src_relative_path.with_stem(file_name).with_suffix(".html"), contents
+        )
+
+
+@dataclass
+class TemplateManager:
+    template_name: str
+    _template_path: Path
+    source_dir: Path
+
+    @classmethod
+    def create(
+        cls,
+        template_path: Path,
+        source_path: Path,
+    ) -> "TemplateManager | None":
+        for folder in source_path.iterdir():
+            if folder.name == template_path.stem:
+                return TemplateManager(
+                    template_path.stem,
+                    template_path.absolute(),
+                    folder.absolute(),
+                )
+
+        logging.warning(
+            f"Unable to find associated files for template {template_path.name}"
+        )
+
+    @staticmethod
+    def _get_files(directory_path: Path):
+        for file in directory_path.iterdir():
+            if file.is_dir():
+                yield from TemplateManager._get_files(file)
+            if file.is_file():
+                yield file.absolute()
+
+    @property
+    def link(self):
+        return "/" + self.template_name
+
+    @property
+    def get_style_file(self) -> Path | None:
+        if (style_path := self._template_path.with_suffix(".css")).exists():
+            return style_path
+
+    def get_category_links(self) -> Dict[Path, Path]:
+        """
+        Returns a dictionary containing links to each page as keys and their absolute path as values.
+        Both the keys and values point to the source files.
+        """
+        page_links = {}
+
+        for page_path in TemplateManager._get_files(self.source_dir):
+            page_link = self.template_name / page_path.relative_to(self.source_dir)
+            if page_link not in page_links:
+                page_links[page_link] = page_path
+
+        return page_links
+
+    def render_files(
+        self, renderer: Environment, mappings: Dict[str, typing.Iterable[str] | str]
+    ):
+        category_links = self.get_category_links()
+
+        # Style file of each template will be copied to the assets folder
+        if style_file := self.get_style_file:
+            mappings["style"] = "assets" + os.path.sep + style_file.name
+
+        mappings["category_sub_links"] = category_links.keys()  # type: ignore
+        template = renderer.get_template(self.template_name)
+
+        rendered_count = 0
+
+        for file_link, file in category_links.items():
+            md_content = md_renderer.render(file.read_text())
+            if md_content is None:
+                logging.warning(f"{file} returned None upon rendering.")
+                continue
+
+            mappings["content"] = md_content
+            contents = template.render(mappings)
+            rendered_count += 1
+            yield RenederedTemplate.create(file_link, contents)
+        
+        logging.info("[Template Manager][%s] has finished rendering %d files", self.template_name, rendered_count)
